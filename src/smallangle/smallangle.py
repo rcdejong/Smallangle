f2b378efc76eeae78c214830ef1366a09ae9f722
rcdejong <rdejong256@gmail.com>
2023-11-17 09:30:15 +0100

    New version sys exit

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -2,27 +2,18 @@ import click
 import numpy as np
 from numpy import pi
 import pandas as pd
-import sys
 
-args = sys.argv
-if(len(args) < 2):
-    func = "sin"
-    num = 9
-
-else: 
-    func = args[1]
-    num = args[2]
-
+#Nodig om sin() en tan() te groeperen
 @click.group()
-def function(function_name, number):
+def func():
+    pass
 
-    if function_name == "sin":
-        sin(number)
-    if function_name == "tan":
-        tan(number)
-
-@function.command()
-@click.argument("number")
+@func.command() #Zorgt ervoor dat je de functie via de command line kan oproepen
+@click.option(
+    "-n",
+    "--number",
+    default=9,
+    ) #Opties voor de argumenten die je (wel of niet) doorgeeft
 def sin(number):
     """List sin up to a given number.
 
@@ -32,13 +23,16 @@ def sin(number):
     Returns:
         list: containing the x and sin
     """
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
-    return
 
-@function.command()
-@click.argument("number")
+@func.command()
+@click.option(
+    "-n",
+    "--number",
+    default=9,
+    )
 def tan(number):
     """List tan up to a given number.
 
@@ -48,11 +42,9 @@ def tan(number):
     Returns:
         list: containing x and tan
     """
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-    return
-    
-if __name__ == "__main__":
-    function()
 
+if __name__ == "__main__":
+    func() #De dummy functie wordt hier opgeroepen omdat we sin() of tan() via de command line oproepen
---
+++ src/smallangle/test.py
@@ -0,0 +1,9 @@
+import sys
+
+args = sys.argv
+#print(args)
+
+if(len(args) < 2):
+    print("No arguments given")
+else:
+    print(args)
