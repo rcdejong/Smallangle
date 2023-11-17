35b53db1d60ea5d50002551ac23d7648d0598f0d
rcdejong <rdejong256@gmail.com>
2023-11-17 09:54:54 +0100

    Cosine added

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -5,46 +5,65 @@ import pandas as pd
 
 #Nodig om sin() en tan() te groeperen
 @click.group()
-def func():
+def gonio_functions():
     pass
 
-@func.command() #Zorgt ervoor dat je de functie via de command line kan oproepen
+@gonio_functions.command() #Zorgt ervoor dat je de functie via de command line kan oproepen
 @click.option(
     "-n",
     "--number",
     default=9,
     ) #Opties voor de argumenten die je (wel of niet) doorgeeft
 def sin(number):
-    """List sin up to a given number.
+    """List sine up to a given number.
 
     Args:
         number (int): number of steps to 2*pi
 
     Returns:
-        list: containing the x and sin
+        list: containing the x and sine
     """
     x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-@func.command()
+@gonio_functions.command()
 @click.option(
     "-n",
     "--number",
     default=9,
     )
 def tan(number):
-    """List tan up to a given number.
+    """List tangent up to a given number.
 
     Args:
         number (int): number of steps to 2*pi
 
     Returns:
-        list: containing x and tan
+        list: containing x and tangent
     """
     x = np.linspace(0, 2 * pi, int(number))
-    df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+    df = pd.DataFrame({"x": x, "tangent (x)": np.tan(x)})
     print(df)
 
+@gonio_functions.command()
+@click.option(
+    "-n",
+    "--number",
+    default=9,
+    )
+def cos(number):
+    """List cosine up to a given number.
+
+    Args:
+        number (int): number of steps to 2*pi
+
+    Returns:
+        list: containing x and cosine
+    """
+    x = np.linspace(0, 2 * pi, int(number))
+    df = pd.DataFrame({"x": x, "cosine (x)": np.cos(x)})
+    print(df)
 if __name__ == "__main__":
-    func() #De dummy functie wordt hier opgeroepen omdat we sin() of tan() via de command line oproepen
+    gonio_functions() #De dummy functie wordt hier opgeroepen omdat we sin() of tan() via de command line oproepen
+
